a73c69a484ac2b0c4cb4d3783e0eadafec91e942
Aditya Pandey <adityapandey3082004@gmail.com>
2024-02-15 17:48:11 +0530

    generics added

--- src/a.ts
+++ src/a.ts
@@ -42,3 +42,54 @@ function isLegal(user: User):boolean{
  }
 
  console.log(isLegal(user))
+
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+function doSomething(keyPressed: Direction) {
+	// do something.
+}
+
+doSomething(Direction.Up)
+
+// common use case in backend
+enum ResponseStatus {
+    Success = 200,
+    NotFound = 404,
+    Error = 500
+}
+
+app.get("/', (req, res) => {
+    if (!req.query.userId) {
+			res.status(ResponseStatus.Error).json({})
+    }
+    // and so on...
+		res.status(ResponseStatus.Success).json({});
+})
+
+//setting default values
+enum Direction {
+    Up = "UP",
+    Down = "Down",
+    Left = "Left",
+    Right = 'Right'
+}
+
+function doSomething(keyPressed: Direction) {
+	// do something.
+}
+
+doSomething(Direction.Down)
+
+// GENERICS
+
+function returnFirstIndex<T> (inputParam: T[]){
+	return inputParam[0];
+}
+
+const a=returnFirstIndex<string>(["Aditya","Pandey"]);
+console.log(a);
